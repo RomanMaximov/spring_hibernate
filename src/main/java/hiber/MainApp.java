7048b5ccde74f5ef26ad07bb0fbce69e4b7aaba5
RomanMaximov <gegenwart.xxx@gmail.com>
2021-11-04 22:21:46 +0300

    new

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,19 +16,27 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User john = new User("John", "Connor", "connor_j@gmail.com");
+      User bobby = new User("Bobby", "Smith", "smith_b@gmail.com");
+      User jimm = new User("Jimm", "Simmons", "simmons_j@gmail.com");
+      User tomas = new User("Tomas", "Brane", "brane_t@gmail.com");
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
+      Car mers = new Car("Mercedes", 500);
+      Car bmw = new Car("BMW", 545);
+      Car toyota = new Car("Toyota", 200);
+      Car audi = new Car("Audi", 8);
+
+      userService.add(john.setCar(mers).setUser(john));
+      userService.add(bobby.setCar(bmw).setUser(bobby));
+      userService.add(jimm.setCar(toyota).setUser(jimm));
+      userService.add(tomas.setCar(audi).setUser(tomas));
+
+      for (User user : userService.listUsers()) {
+         System.out.println(user + " " + user.getCar());
          System.out.println();
       }
+
+      System.out.println(userService.getUserByCar("Audi", 8));
 
       context.close();
    }
